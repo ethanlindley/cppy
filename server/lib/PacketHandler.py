29a78bb4d1275418e7e357e681824c3050b90b75
Ethan Lindley <ethan@legacystudios.co>
2018-05-08 22:52:58 -0500

    general: cleanup

--- server/lib/PacketHandler.py
+++ server/lib/PacketHandler.py
@@ -1,20 +1,19 @@
 import logging
 from twisted.internet.protocol import DatagramProtocol
 
+log = logging.getLogger(__name__)
+
 
 class PacketHandler(DatagramProtocol):
-    def __init__(self):
-        self.logger = logging.getLogger(type(self).__name__)
-
     def handleReceivedData(self, data, address):
         data = str(data)
 
         if data[0] == "<":
-            self.logger.debug("received XML packet from addr {} with data :: {}".format(repr(address), repr(data)))
+            log.debug("received XML packet from addr {} with data :: {}".format(repr(address), repr(data)))
             # TODO: do something with packet
             # data = self.handleXMLPacket(data)
         elif data[0] == "%":
-            self.logger.debug("received RAW packet from addr {} with data :: {}".format(repr(address), repr(data)))
+            log.debug("received RAW packet from addr {} with data :: {}".format(repr(address), repr(data)))
             # TODO: do something with packet
             # data = self.handleRAWPacket(data)
 
@@ -22,8 +21,8 @@ class PacketHandler(DatagramProtocol):
 
     def handleXMLPacket(self, packet):
         # TODO: handle received packet properly
-        return None
+        return packet
     
     def handleRAWPacket(self, packet):
         # TODO: handle received packet properly
-        return None
+        return packet
--- server/login/LoginProtocol.py
+++ server/login/LoginProtocol.py
@@ -3,24 +3,26 @@ from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 from server.lib.PacketHandler import PacketHandler
 
+log = logging.getLogger(__name__)
+
 
 class LoginProtocol(DatagramProtocol):
     def __init__(self, users=None):
-        self.logger = logging.getLogger(type(self).__name__)
         self.users = users
 
     def connectionMade(self):
         # let's keep track and increment the number of users 
         # currently connected to the server
-        self.logger.info("user connected")
+        log.info("user connected")
         self.users = self.users + 1
     
     def connectionLost(self, reason):
         # keeping track of users
-        self.logger.info("user disconnected with reason {}".format(reason))
+        log.info("user disconnected with reason {}".format(reason))
         self.users = self.users - 1
 
     def datagramReceived(self, datagram, address):
+        log.debug("datagram received from {}".format(repr(address)))
         ph = PacketHandler()
         ph.handleReceivedData(datagram, address)
 
@@ -28,7 +30,7 @@ class LoginProtocol(DatagramProtocol):
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # set the port to non-blocking
 sock.setblocking(False)
-sock.bind(('127.0.0.1', 6112))
+sock.bind(('0.0.0.0', 6112))
 
 # let's pass the port file descriptor to the reactor
 port = reactor.adoptDatagramPort(sock.fileno(), socket.AF_INET, LoginProtocol())

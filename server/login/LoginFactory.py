7dd7c73e9e44acddb99219e76e1325660c7e126b
Ethan Lindley <ethan@legacystudios.co>
2018-05-09 01:30:45 -0500

    login: fix LoginFactory related issue

--- server/login/LoginFactory.py
+++ server/login/LoginFactory.py
@@ -2,7 +2,6 @@ import sys
 from .LoginProtocol import LoginProtocol
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor, protocol
-from twisted.application import service
 from logbook import Logger, StreamHandler
 
 StreamHandler(sys.stdout).push_application()
@@ -10,10 +9,8 @@ log = Logger(__name__)
 
 
 class LoginFactory(protocol.Factory):
-    protocol = LoginProtocol
-
     def buildProtocol(self, address):
-        return protocol
+        return LoginProtocol()
 
 
 port = 6112
